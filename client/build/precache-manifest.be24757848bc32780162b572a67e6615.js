f65eaa18b9f77a720d69aa5c01bc21e94c7a3c92
Cassia Melvin <cassiamelvin@gmail.com>
2020-07-31 19:10:53 -0400

    Prepare for deploy p2

---
+++ client/build/precache-manifest.be24757848bc32780162b572a67e6615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5cbb929333debcbb75a56b99bc231a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ddcf44641be8552a84b",
+    "url": "/static/css/main.e3270efa.chunk.css"
+  },
+  {
+    "revision": "27a06230c8c5c7e14e9c",
+    "url": "/static/js/2.c4232f30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4232f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ddcf44641be8552a84b",
+    "url": "/static/js/main.50acb7be.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
